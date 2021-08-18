91cdfb710a784f8ed9b20c59a55613c77abdd51d
GKObakhidze <kobakhidzegiga@gmail.com>
2021-08-18 18:16:46 +0400

    new commit

--- src/test/java/Arrays_of_Strings.java
+++ src/test/java/Arrays_of_Strings.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Arrays_of_Strings {
+public class Arrays_of_Strings {
+    public static void main(String[] args) {
+        String[] words = new String[3];
+
+        words[0] = "Hello";
+        words[1] = "to";
+        words[2] = "you";
+
+        System.out.println(words[2]);
+
+        String[] fruits = {"apple", "banana", "pear", "kiwi"};
+
+        for (String fruit: fruits){
+            System.out.println(fruit);
+            System.out.println(fruit);
+            System.out.println(fruit);
+        }
+
+
+
+
+    }
+
+
 }
--- src/test/java/Switch.java
+++ src/test/java/Switch.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Switch {
-    public void main(String[] args){
+    public static void  main(String[] args){
 
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a commnad: ");
