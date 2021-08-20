413983e3640467163d96fee7c4d16733a46bffea
GKObakhidze <kobakhidzegiga@gmail.com>
2021-08-20 10:14:44 +0400

    wrapper clas

---
+++ src/test/java/wrapper_clsses.java
@@ -0,0 +1,19 @@
+public class wrapper_clsses {
+    public static void main(String[] args) {
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c =123;
+        Double d = 3.14;
+        String e ="Bro";
+
+        if(a==true){
+            System.out.println("This is true");
+        }
+
+
+
+
+    }
+
+}
