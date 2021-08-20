597888a1cefc93e7a50985cc946933a433e6b4cf
GKObakhidze <kobakhidzegiga@gmail.com>
2021-08-20 14:08:21 +0400

    methods

---
+++ src/test/java/methods.java
@@ -0,0 +1,28 @@
+public class methods {
+    public static void main(String[] args) {
+
+        /*String name = "giga";
+        int age = 21;
+
+        hello(name, age);*/
+
+        int x=3;
+        int y =4;
+        int z = add(x,y);
+        System.out.println(z);
+
+    }
+           //
+    /*static void hello(String title,int age) {
+        System.out.println("hello "+ title);
+        System.out.println("You are  "+ age);
+    }*/
+          //
+    static int add(int x,int y) {
+        //int z = x+y;
+        int z = x+y;
+        return z;
+    }
+
+
+}
