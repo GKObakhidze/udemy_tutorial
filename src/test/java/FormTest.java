ea07e78bd01ff7424cc22c3f70f2a3ae5c263d9f
user1 <kobakhidzegiga@gmail.com>
2021-08-20 14:54:02 +0400

    files

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class FormTest {
+import javax.swing.*;
+
+public class FormTest {
+    private JButton submitButton;
 }
--- src/test/java/final_keyword.java
+++ src/test/java/final_keyword.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class final_keyword {
+public class final_keyword {
+      public static void main(String[] args) {
+
+          final double PI = 3.14159;
+          // final mean that PI value not changeable
+          // PI = 4;
+          System.out.println(PI);
+
+
+      }
+
+
 }
