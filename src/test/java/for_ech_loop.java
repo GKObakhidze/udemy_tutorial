cb760c69aa73ac91f4efa0c38e3a5de2a726163d
GKObakhidze <kobakhidzegiga@gmail.com>
2021-08-20 14:08:02 +0400

    for_ech loop

---
+++ src/test/java/for_ech_loop.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class for_ech_loop {
+    public static void main(String[] args) {
+
+       // String[] animals = {"cat","dog", "rat","bird"};
+
+        ArrayList<String> animals = new ArrayList<String>();
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("rat");
+        animals.add("bird");
+
+        for (String i: animals){
+            System.out.println(i);
+
+        }
+
+
+
+
+
+
+
+    }
+}
