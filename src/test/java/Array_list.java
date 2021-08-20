467a6747bcd59088b6a54615b2d2304c6f8eb885
GKObakhidze <kobakhidzegiga@gmail.com>
2021-08-20 10:40:57 +0400

    Arrays_list

--- src/test/java/Array_list.java
+++ src/test/java/Array_list.java
@@ -8,6 +8,11 @@ public class Array_list {
         food.add("pizza");
         food.add("hamburger");
         food.add("hotdog");
+
+        food.set(0,"sushi");
+        food.remove(2);
+        food.clear();
+
         for (int i=0; i<food.size(); i++) {
             System.out.println(food.get(i));
         }
