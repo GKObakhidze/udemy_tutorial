3d994c6058675bc9f9ca0e414023edd54e110290
GKObakhidze <kobakhidzegiga@gmail.com>
2021-09-06 23:31:21 +0400

    access modifiers

---
+++ src/test/java/package1/B.java
@@ -0,0 +1,17 @@
+package package1;
+import package2.*;
+
+public class B {
+
+    public static void main(String[] args){
+        B b= new B();
+//        System.out.println(b.privateMessage);
+
+
+
+    }
+
+
+
+
+}
